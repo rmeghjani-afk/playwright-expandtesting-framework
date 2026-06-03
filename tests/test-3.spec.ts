c7c2d1394f182609167b7d661d01eab12b44c5d9
Playwright User <playwright@example.com>
2026-06-03 11:18:35 -0600

    Another Commit 3rd June 2026

--- tests/test-3.spec.ts
+++ tests/test-3.spec.ts
@@ -1,12 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('test', async ({ page }, testInfo) => {
+  testInfo.setTimeout(60000);
+
   await page.goto('https://automationexercise.com/');
   await page.getByRole('button', { name: 'Test Cases' }).click();
   await page.getByRole('link', { name: 'Test Case 1: Register User' }).click();
   await page.locator('#collapse1').getByText('Launch browser').click();
   await page.getByRole('link', { name: ' Products' }).click();
-  await page.locator('iframe[name="aswift_2"]').contentFrame().getByRole('button', { name: 'Close ad' }).click();
   await page.getByRole('link', { name: ' Signup / Login' }).click();
   await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').click();
   await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').fill('test@test.com');
